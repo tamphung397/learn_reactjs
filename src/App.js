bf8d06cbf494f3d9b47e855c29b1a519ccb5fb8d
tam phung <tamphung397@gmail.com>
2021-04-03 19:06:50 +0700

    third

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn more reactjs
+          Learn more reactjs version 2
         </a>
       </header>
     </div>
